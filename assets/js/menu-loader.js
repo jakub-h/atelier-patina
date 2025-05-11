433ceb555c63d10be1e64095e2bf2a454e6ecab7
Jakub Hruska <jakub.hruska@innovatrics.com>
2025-05-11 14:14:42 +0200

    Fix menu paths

--- assets/js/menu-loader.js
+++ assets/js/menu-loader.js
@@ -16,6 +16,13 @@ document.addEventListener("DOMContentLoaded", function() {
     .then(response => response.text())
     .then(data => {
         document.getElementById('navbar').innerHTML = data;
+
+        // Update all links with data-base-path attribute
+        document.querySelectorAll('a[data-base-path]').forEach(link => {
+            const currentHref = link.getAttribute('href');
+            link.setAttribute('href', `${siteBase}/${currentHref}`);
+        });
+
         // Remove is-preload as soon as menu is ready
         document.body.classList.remove('is-preload');
         // Now that the menu is loaded, load your main JS
