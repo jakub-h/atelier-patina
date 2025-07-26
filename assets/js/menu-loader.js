1598cf1d59a4085d2cf466857d051f5bcd6ad793
Jakub Hruska <jakub.hruska@innovatrics.com>
2025-07-26 19:59:32 +0200

    Add navbar to all pages

--- assets/js/menu-loader.js
+++ assets/js/menu-loader.js
@@ -15,6 +15,21 @@ document.addEventListener("DOMContentLoaded", function() {
     fetch(`${siteBase}/templates/menu.html`)
     .then(response => response.text())
     .then(data => {
+        // Check if this is the index page (has banner)
+        const isIndexPage = document.getElementById('banner') !== null;
+
+        // If it's the index page, add the 'alt' class to preserve animation
+        if (isIndexPage) {
+            data = data.replace('<header id="header">', '<header id="header" class="alt">');
+        }
+
+        // Fix logo path for gallery pages (they're in gallery_pages/ subdirectory)
+        const currentPath = window.location.pathname;
+        if (currentPath.includes('/gallery_pages/')) {
+            // Gallery pages need to go up one directory level for images
+            data = data.replace('src="images/logo.svg"', 'src="../images/logo.svg"');
+        }
+
         document.getElementById('navbar').innerHTML = data;
 
         // Update all links with data-base-path attribute
